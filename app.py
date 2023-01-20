97ab77ec18c24d329a983e93d79b8c1cdbc6c285
teguhpambudif <teguhpambudi1251@gmail.com>
2023-01-21 01:38:21 +0700

    setting up, get error when create_all() db

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'normies'
+# setup context
+app.app_context()
+# initialize db
+db.init_app(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),nullable=False)
+    password = db.Column(db.String(80),nullable=False)
 
 @app.route('/')
 def home():
